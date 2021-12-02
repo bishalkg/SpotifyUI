db6bab7315b1b606679091564507e71a05741ab0
Bishal Gautam <bgautam.se@gmail.com>
2021-12-01 20:36:59 -0800

    set up nextauth

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,37 +1,70 @@
 import { HomeIcon, SearchIcon, LibraryIcon, PlusCircleIcon, RssIcon, HeartIcon} from '@heroicons/react/outline';
 
-function Sidebar() {
+const Sidebar = () => {
   return (
     <div className="text-gray-500 p-5 text-sm border-r border-gray-900">
       <div className="space-y-4">
-        <button className="flex items-center space-x-2 hover:text-white">
+        <button className="flex items-center space-x-2 hover:text-green-500">
           <HomeIcon className="h-5 w-5"/>
           <p>Home</p>
         </button>
-        <button className="flex items-center space-x-2 hover:text-white">
+        <button className="flex items-center space-x-2 hover:text-green-500">
           <SearchIcon className="h-5 w-5"/>
           <p>Search</p>
         </button>
-        <button className="flex items-center space-x-2 hover:text-white">
+        <button className="flex items-center space-x-2 hover:text-green-500">
           <LibraryIcon className="h-5 w-5"/>
           <p>Your Library</p>
         </button>
         <hr className="border-t-[0.1px] border-gray-900"/>
 
-        <button className="flex items-center space-x-2 hover:text-white">
+        <button className="flex items-center space-x-2 hover:text-green-500">
           <PlusCircleIcon className="h-5 w-5"/>
           <p>Create Playlist</p>
         </button>
-        <button className="flex items-center space-x-2 hover:text-white">
+        <button className="flex items-center space-x-2 hover:text-green-500">
           <HeartIcon className="h-5 w-5"/>
           <p>Liked Songs</p>
         </button>
-        <button className="flex items-center space-x-2 hover:text-white">
+        <button className="flex items-center space-x-2 hover:text-green-500">
           <RssIcon className="h-5 w-5"/>
           <p>Your episodes</p>
         </button>
         <hr className="border-t-[0.1px] border-gray-900"/>
         {/* Playlists */}
+        <p className="cursor-pointer hover:text-white">
+          Playlist names...
+        </p>
+        <p className="cursor-pointer hover:text-white">
+          Playlist names...
+        </p>
+        <p className="cursor-pointer hover:text-white">
+          Playlist names...
+        </p>
+        <p className="cursor-pointer hover:text-white">
+          Playlist names...
+        </p>
+        <p className="cursor-pointer hover:text-white">
+          Playlist names...
+        </p>
+        <p className="cursor-pointer hover:text-white">
+          Playlist names...
+        </p>
+        <p className="cursor-pointer hover:text-white">
+          Playlist names...
+        </p>
+        <p className="cursor-pointer hover:text-white">
+          Playlist names...
+        </p>
+        <p className="cursor-pointer hover:text-white">
+          Playlist names...
+        </p>
+        <p className="cursor-pointer hover:text-white">
+          Playlist names...
+        </p>
+        <p className="cursor-pointer hover:text-white">
+          Playlist names...
+        </p>
       </div>
     </div>
   )
