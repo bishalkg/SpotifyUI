6c944a15baad52a4ab0ed3e83b8b9749ac515101
Bishal Gautam <bgautam.se@gmail.com>
2021-12-07 21:54:40 -0800

    player works

--- components/Player.js
+++ components/Player.js
@@ -1,9 +1,12 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useSession } from 'next-auth/react';
 import { useRecoilState } from 'recoil';
 import { currentTrackIdState, isPlayingState } from '../atoms/songAtom';
 import useSpotify from '../hooks/useSpotify';
 import useSongInfo from '../hooks/useSongInfo';
+
+import { HeartIcon, VolumeUpIcon as VolumeDownIcon } from '@heroicons/react/outline';
+import { FastForwardIcon, PauseIcon, PlayIcon, ReplyIcon, RewindIcon, VolumeUpIcon, SwitchHorizontalIcon } from '@heroicons/react/solid';
 
 function Player() {
 
@@ -14,14 +17,78 @@ function Player() {
   const [volume, setVolume] = useState(50);
 
   const songInfo = useSongInfo();
+  const fetchCurrentSong = () => {
+    if (!songInfo) {
+      spotifyApi.getMyCurrentPlayingTrack().then(data => {
+        setCurrentTrackId(data.body?.item?.id);
+
+        spotifyApi.getMyCurrentPlaybackState().then((data) => {
+          console.log('now playing', data.body);
+          setIsPlaying(data.body?.is_playing);
+        });
+      });
+    }
+  }
+  const handlePlayPause = () => {
+    spotifyApi.getMyCurrentPlaybackState().then((data) => {
+      if (data.body.is_playing) {
+        spotifyApi.pause();
+        setIsPlaying(false);
+      } else {
+        spotifyApi.play();
+        setIsPlaying(true);
+      }
+    });
+  };
+
+  useEffect(() => {
+    if (spotifyApi.getAccessToken() && !currentTrackId) {
+
+      fetchCurrentSong();
+      setVolume(50);
+    }
+
+  }, [currentTrackIdState, spotifyApi, session])
 
   return(
-    <div>
+    <div className="h-24 bg-gradient-to-b from-black to-gray-900 text-white grid grid-cols-3 text-xs md:text-base px-2 md:px-8">
+      <div className="flex items-center space-x-4">
+        <img className="hidden md:inline h-10 w-10" src={songInfo?.album.images?.[0]?.url} alt=""/>
       <div>
-        <img className="hidden md:inline h-10 w-10" src={songInfo?.album.images?.[0]?.url} alt=""></img>
+        <h3>{songInfo?.name}</h3>
+        <p>{songInfo?.artists?.[0]?.name}</p>
+      </div>
+      </div>
+
+      <div className="flex items-center justify-evenly">
+        <SwitchHorizontalIcon className="button" />
+        <RewindIcon
+          //onClick
+          className="button"
+        />
+
+        {isPlaying ? (
+          <PauseIcon onClick={handlePlayPause} className="button w-10 h-10" />
+        ) : (
+          <PlayIcon onClick={handlePlayPause} className="button w-10 h-10"/>
+        )}
+
+        <FastForwardIcon
+          //onClick
+          className="button"
+        />
+        <ReplyIcon className="button" />
+
+      </div>
+
+      <div className="flex items-center space-x-3 md:space-x-4 justify-end">
+        <VolumeDownIcon className="button"/>
+        <input className="w-14 md:w-28" type="range" value="" min={0} max={100}/>
+        <VolumeUpIcon className="button"/>
       </div>
     </div>
   )
 }
 
 export default Player;
+
--- components/Sidebar.js
+++ components/Sidebar.js
@@ -27,7 +27,7 @@ const Sidebar = () => {
 
   // console.log(session)
   return (
-    <div className="text-gray-500 p-5 text-sm lg:text-sm border-r border-gray-900 overflow-y-scroll scrollbar-hide h-screen sm:max-w-[12rem] lg:max-w-[15rem] hidden md:inline-flex">
+    <div className="text-gray-500 p-5 text-sm lg:text-sm border-r border-gray-900 overflow-y-scroll scrollbar-hide h-screen sm:max-w-[12rem] lg:max-w-[15rem] hidden md:inline-flex pb-36">
       <div className="space-y-4">
       <button onClick={() => signOut()} className="flex items-center space-x-2 hover:text-green-500">
           <p>Logout</p>
