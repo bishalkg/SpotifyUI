8cfe9c057c0cec019abf67a152959117fd96ff58
Bishal Gautam <bgautam.se@gmail.com>
2021-12-07 21:07:16 -0800

    useSongInfo hook

--- components/Player.js
+++ components/Player.js
@@ -1,8 +1,24 @@
+import { useState } from 'react';
+import { useSession } from 'next-auth/react';
+import { useRecoilState } from 'recoil';
+import { currentTrackIdState, isPlayingState } from '../atoms/songAtom';
+import useSpotify from '../hooks/useSpotify';
+import useSongInfo from '../hooks/useSongInfo';
+
 function Player() {
+
+  const spotifyApi = useSpotify();
+  const { data: session, status } = useSession();
+  const [currentTrackId, setCurrentTrackId] = useRecoilState(currentTrackIdState);
+  const [isPlaying, setIsPlaying] = useRecoilState(isPlayingState);
+  const [volume, setVolume] = useState(50);
+
+  const songInfo = useSongInfo();
+
   return(
     <div>
       <div>
-
+        <img className="hidden md:inline h-10 w-10" src={songInfo?.album.images?.[0]?.url} alt=""></img>
       </div>
     </div>
   )
